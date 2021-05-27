83b3b45118072f1c3a866b5cdc7a22d93fdc03f4
Jonghyun Min <straightmin@gmail.com>
2021-05-27 21:56:10 +0900

    서버, 클라이언트 메인 소스 파일 생성

---
+++ OSS-Client/src/Operator.java
@@ -0,0 +1,14 @@
+/**
+ * [GUI 기반 화상통화 프로그램]
+ * Operator: 클라이언트 단에서 모든 프레임을 관리하는 클래스
+ * 모든 클래스를 하나의 인스턴스로 묶어서 관리한다.
+ */
+
+public class Operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ OSS-Server/src/Server.java
@@ -0,0 +1,14 @@
+/**
+ * [GUI 기반 화상통화 프로그램]
+ * Server: 통화 프로그램 메인 서버
+ * 클라이언트 접속 대기, 클라이언트 객체 저장 및 관리
+ */
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
