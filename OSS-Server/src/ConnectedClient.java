44b58d1b2a607dda8acc7bda78ec0da0070d974b
82107 <82107@DESKTOP-HK00K2R>
2021-05-27 23:30:26 +0900

    서버완성

--- OSS-Server/src/ConnectedClient.java
+++ OSS-Server/src/ConnectedClient.java
@@ -1,8 +1,16 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.StringTokenizer;
+
+/**
+ * [GUI 기반 화상통화 프로그램] ConnectedClient: 접속 클라이언트 저장용 스레드 각 스레드는 클라이언트와 1대1로
+ * 소켓통신하며 클라이언트가 송신한 메시지를 수신하여 구분한 후 메시지에 알맞는 동작을 수행한다.
+ */
 
 public class ConnectedClient extends Thread {
 	Socket socket;
@@ -28,7 +36,94 @@ public class ConnectedClient extends Thread {
 	}
 
 	public void run() {
+		try {
+			System.out.println("Server > " + this.socket.toString() + "에서의 접속이 연결되었습니다.");
+			outStream = this.socket.getOutputStream();
+			dataOutStream = new DataOutputStream(outStream);
+			inputStream = this.socket.getInputStream();
+			dataInStream = new DataInputStream(inputStream);
+
+			// 접속 동작
+			String users = "";
+			for (ConnectedClient cc : server.clients) { // 접속 회원 리스트 작성
+				if (cc.uName == null)
+					continue;
+				users += TK + cc.uName;
+			}
+			for (ConnectedClient cc : server.clients) { // 접속 메시지, 회원 리스트 전달
+				// 클라이언트 측에서 채팅방에 입장한 회원 파악 위해 이름 전달
+				outStream = cc.socket.getOutputStream();
+				dataOutStream = new DataOutputStream(outStream);
+				dataOutStream.writeUTF(connectTag + TK + uName);
+				dataOutStream.writeUTF(listTag + users);
+			}
+
+			// 모든 클라이언트에 접근한 후 엉뚱한 곳에 연결되기 때문에 아웃풋 스트림 재설정
+			outStream = this.socket.getOutputStream();
+			dataOutStream = new DataOutputStream(outStream);
+
+			while (true) {
+				msg = dataInStream.readUTF();
+				StringTokenizer st = new StringTokenizer(msg, TK);
+				String control = st.nextToken(); // 명령메시지 분리, switch를 통해 기능 구분
+
+				switch (control) { // 명령 메시지에 따라 기능 구분
+				case chatTag: // 채팅 메시지
+					msg = st.nextToken();
+
+					// 송신한 클라이언트를 포함 모든 클라이언트에게 채팅 메시지 전달
+					for (ConnectedClient cc : server.clients) {
+						outStream = cc.socket.getOutputStream();
+						dataOutStream = new DataOutputStream(outStream);
+						dataOutStream.writeUTF(chatTag + TK + uName + " : " + msg);
+					}
+
+					break;
+				case QuitTag: // 클라이언트종료
+
+					// 다른 모든 클라이언트에게 클라이언트종료시 정보 전달
+					for (ConnectedClient cc : server.clients) {
+						if (uName.equals(cc.uName))
+							continue;
+						outStream = cc.socket.getOutputStream();
+						dataOutStream = new DataOutputStream(outStream);
+						dataOutStream.writeUTF(QuitTag + TK + uName);
+					}
+					uName = null; // 회원 리스트 무결성을 위해 클라이언트종료시 이름 비우기
+					break;
+				}
+				// switch(control)
+
+				// 모든 클라이언트에 접근한 후 엉뚱한 곳에 연결되기 때문에 아웃풋 스트림 재설정
+				outStream = this.socket.getOutputStream();
+				dataOutStream = new DataOutputStream(outStream);
+			}
+			// while(true)
+
+		} catch (SocketException e) {
+			System.out.println("Server > " + this.socket.toString() + "에서의 접속이 해제되었습니다.");
+		} catch (IOException e) {
+			System.out.println("Server > 입출력 관련 예외 발생");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("최종? 예외");
+			e.printStackTrace();
+		} finally {
+			// 오류 또는 클라이언트 종료로 인해 소켓 연결이 해제되면 서버와 연결된 클라이언트 정보 삭제, 모든 스트림 페쇄
+			server.clients.remove(server.clients.indexOf(this));
+			try {
+				dataInStream.close();
+				inputStream.close();
+				dataOutStream.close();
+				outStream.close();
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		// try ~ catch
 
 	}
+	// run()
 
 }
--- OSS-Server/src/Server.java
+++ OSS-Server/src/Server.java
@@ -11,23 +11,24 @@ import java.util.ArrayList;
  */
 
 public class Server {
-
+	// 서버소켓
 	ServerSocket ss = null;
-
+	// 접속한 클라이언트 정보들을 어레이리스트로 여러개 저장 및 관리
 	ArrayList<ConnectedClient> clients = new ArrayList<ConnectedClient>();
-
+	// 포트번호 상수. 7777번으로 설정
 	final static int portNum = 7777;
 
+	// 메인 메소드. 클라이언트 접속 상시대기, 클라이언트 스레드 저장 및 동작
 	public static void main(String[] args) {
 
-		int count = 0;
+		int count = 0; // 접속한 인원 구분용 카운트 변수
 		Server server = new Server();
 
 		try {
-			server.ss = new ServerSocket(portNum);
+			server.ss = new ServerSocket(portNum);  //포트개방
 			System.out.println("Server > Server Socket is Created ...");
 			while (true) {
-
+				// 클라이언트 접속 대기
 				Socket socket = server.ss.accept();
 				ConnectedClient c = new ConnectedClient(socket, server, count++);
 				server.clients.add(c);
